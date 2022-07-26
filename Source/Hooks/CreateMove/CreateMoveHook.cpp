a4e20ca35aa42eb6066021174926649b9e2e7ce3
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-07-26 20:16:13 +0200

    Move the CreateMove Hooking logic into CreateMoveHook.cpp

--- Source/Hooks/CreateMove/CreateMoveHook.cpp
+++ Source/Hooks/CreateMove/CreateMoveHook.cpp
@@ -1,10 +1,13 @@
 #include "ReturnAddr/ReturnAddr.hpp"
+#include "Hooking/Hooking.hpp"
+#include "PatternScan/PatternScan.hpp"
 
 #include "CreateMoveHook.hpp"
 #include "../../Interfaces.hpp"
 
 #include "../../SDK/InputFlags.hpp"
 #include "../../SDK/StateFlags.hpp"
+#include "../../Utils/VMT.hpp"
 
 #include <cstdint>
 #include <cstdio>
@@ -37,4 +40,23 @@ bool Hooks::CreateMove::CreateMoveHook(void* thisptr, float flInputSampleTime, C
 	}
 
 	return silent || true;
+}
+
+void Hooks::CreateMove::Hook() {
+	void* hudProcessInput = Utils::GetVTable(Interfaces::baseClient)[10];
+	
+	/**
+	 * This method calls GetClientMode()
+	 * Call instruction is at the 11th byte
+	 * The next byte is the address
+	 */
+	char* relAddr = static_cast<char*>(hudProcessInput) + 12;
+	void* getClientMode = relAddr + 4 + *reinterpret_cast<int*>(relAddr);
+	
+	relAddr = static_cast<char*>(getClientMode) + 4; // Skip push and mov opcodes
+	void* clientMode = *(void**) (relAddr + 4 + *reinterpret_cast<int*>(relAddr));
+	
+	void* createMove = Utils::GetVTable(clientMode)[25];
+
+	proxy = Framework::Hooking::detour(createMove, (void*) CreateMoveHook, 6);
 }
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,10 +1,5 @@
-#include "ReturnAddr/ReturnAddr.hpp"
-#include "Hooking/Hooking.hpp"
-#include "PatternScan/PatternScan.hpp"
-
-#include "Interfaces.hpp"
 #include "Hooks/CreateMove/CreateMoveHook.hpp"
-#include "Utils/VMT.hpp"
+#include "Interfaces.hpp"
 
 #include <cstdio>
 
@@ -13,24 +8,8 @@ int __attribute((constructor)) Startup() {
 	Interfaces::baseClient = Interfaces::GetInterface("./csgo/bin/linux64/client_client.so", "VClient");
 	Interfaces::engine = static_cast<CEngineClient*>(Interfaces::GetInterface("./bin/linux64/engine_client.so", "VEngineClient"));
 	Interfaces::entityList = static_cast<CClientEntityList*>(Interfaces::GetInterface("./csgo/bin/linux64/client_client.so", "VClientEntityList"));
-	
-	void* hudProcessInput = Utils::GetVTable(Interfaces::baseClient)[10];
-	
-	/**
-	 * This method calls GetClientMode()
-	 * Call instruction is at the 11th byte
-	 * The next byte is the address
-	 */
-	char* relAddr = static_cast<char*>(hudProcessInput) + 12;
-	void* getClientMode = relAddr + 4 + *reinterpret_cast<int*>(relAddr);
-	
-	relAddr = static_cast<char*>(getClientMode) + 4; // Skip push and mov opcodes
-	void* clientMode = *(void**) (relAddr + 4 + *reinterpret_cast<int*>(relAddr));
-	
-	void* createMove = Utils::GetVTable(clientMode)[25];
 
-	Hooks::CreateMove::proxy = Framework::Hooking::detour(createMove, (void*) Hooks::CreateMove::CreateMoveHook, 6);
-	printf("CreateMove has been detour'd\n");
-
+	Hooks::CreateMove::Hook();
+	
 	return 0;
 }
