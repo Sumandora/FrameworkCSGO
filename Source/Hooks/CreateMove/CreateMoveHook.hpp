db6bbd719ad231717045e6f83230787e2074b0eb
Sumandora <>
2022-07-26 15:09:51 +0200

    Reorganized the base into seperate files

---
+++ Source/Hooks/CreateMove/CreateMoveHook.hpp
@@ -0,0 +1,13 @@
+#ifndef HOOKS_CREATEMOVE_HOOK
+#define HOOKS_CREATEMOVE_HOOK
+
+#include "../../SDK/CUserCmd.hpp"
+
+namespace Hooks {
+	namespace CreateMove {
+		inline void* proxy;
+		bool CreateMoveHook(void*, float, CUserCmd*);
+	}
+}
+
+#endif
---
+++ Source/SDK/Vector.hpp
@@ -0,0 +1,8 @@
+#ifndef SDK_VECTOR
+#define SDK_VECTOR
+
+struct Vector {
+	float x, y, z;
+};
+
+#endif
