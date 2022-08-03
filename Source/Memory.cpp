36748b1c098b8866c3e18d5dff92ee0fc4efe019
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-08-03 19:11:00 +0200

    Remove some unused left-overs and fix a compiler error

--- Source/Memory.cpp
+++ Source/Memory.cpp
@@ -36,25 +36,4 @@ void Memory::Create() {
 	
 	Interfaces::clientMode	= *reinterpret_cast<void**>(relToAbsAddress(static_cast<char*>(getClientMode) + 4));
 	Interfaces::globalVars	= *reinterpret_cast<CGlobalVars**>(relToAbsAddress(static_cast<char*>(hudUpdate) + 16));
-	
-	// This method can be used to simulate recoil
-	// https://github.com/SwagSoftware/Kisak-Strike/blob/7df358a4599ba02a4e072f8167a65007c9d8d89c/game/shared/cstrike15/weapon_csbase.cpp#L3838
-	csweaponbase_recoil =
-		Pattern(xorstr_(
-			"\x55"				// push %rbp
-			"\x48\x89\xe5"		// mov %rsp,%rsp
-			"\x41\x57"			// push %r15
-			"\x41\x56"			// push %r14
-			"\x41\x55"			// push %r13
-			"\x41\x89\xf5"		// mov %esi,%r13d
-			"\x41\x54"			// push %r12
-			"\x53"				// push %rbx
-			"\x48\x89\xfb"		// mov %rdi,%rbx
-			"\x48\x83\xec\x18"	// sub $0x18,%rsp ; Because we are ensuring the method prolog here, we can hardcode this number, because it is depending on the prolog
-			"\xe8\00\00\00\00"	// call fun_XXXXXXXX ; This will change
-			"\x48\x85\xc0"		// test %rax,%rax
-			), xorstr_("xxxxxxxxxxxxxxxxxxxxxxxx????xxx")
-		).searchPattern(
-			GetBaseAddress(xorstr_("./csgo/bin/linux64/client_client.so"))
-		);
 }
