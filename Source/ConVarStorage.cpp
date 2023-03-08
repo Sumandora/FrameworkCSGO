69e0601b6e7a10fff98c2966003ce2f85183cc88
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-03-08 14:07:32 +0100

    Disable ImGUI Asserts in Release Mode to save performance

--- Source/ConVarStorage.cpp
+++ Source/ConVarStorage.cpp
@@ -1,12 +1,14 @@
 #include "ConVarStorage.hpp"
 
+#include "xorstr.hpp"
+
 #include "Interfaces.hpp"
 
 void ConVarStorage::FindAll() {
-    weapon_recoil_scale			= Interfaces::icvar->FindVar("weapon_recoil_scale");
-    mp_teammates_are_enemies	= Interfaces::icvar->FindVar("mp_teammates_are_enemies");
+    weapon_recoil_scale			= Interfaces::icvar->FindVar(xorstr_("weapon_recoil_scale"));
+    mp_teammates_are_enemies	= Interfaces::icvar->FindVar(xorstr_("mp_teammates_are_enemies"));
 
-    game_type		= Interfaces::icvar->FindVar("game_type");
-    game_mode		= Interfaces::icvar->FindVar("game_mode");
-    sv_skirmish_id	= Interfaces::icvar->FindVar("sv_skirmish_id");
+    game_type		= Interfaces::icvar->FindVar(xorstr_("game_type"));
+    game_mode		= Interfaces::icvar->FindVar(xorstr_("game_mode"));
+    sv_skirmish_id	= Interfaces::icvar->FindVar(xorstr_("sv_skirmish_id"));
 }
