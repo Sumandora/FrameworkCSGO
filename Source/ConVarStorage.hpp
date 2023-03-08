bdc51214b79b6efe08bdae82676b813a66b4aac4
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-03-08 17:34:37 +0100

    Spaces -> Tabs

--- Source/ConVarStorage.hpp
+++ Source/ConVarStorage.hpp
@@ -4,14 +4,14 @@
 #include "SDK/GameClass/ConVar.hpp"
 
 namespace ConVarStorage {
-    inline ConVar* weapon_recoil_scale;
-    inline ConVar* mp_teammates_are_enemies;
+	inline ConVar* weapon_recoil_scale;
+	inline ConVar* mp_teammates_are_enemies;
 
-    inline ConVar* game_type;
-    inline ConVar* game_mode;
-    inline ConVar* sv_skirmish_id;
-    
-    void FindAll();
+	inline ConVar* game_type;
+	inline ConVar* game_mode;
+	inline ConVar* sv_skirmish_id;
+
+	void FindAll();
 }
 
 #endif
