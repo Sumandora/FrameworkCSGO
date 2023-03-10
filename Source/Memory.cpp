e722bf8b1f288a64d770787e5c3c4dfa570986b3
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-03-10 19:48:42 +0100

    Remove desperate debugging attempts

--- Source/Memory.cpp
+++ Source/Memory.cpp
@@ -10,47 +10,6 @@
 #include "PatternScan/PatternScan.hpp"
 
 #include "Utils/VMT.hpp"
-struct dlinfo_t {
-	const char* library = nullptr;
-	uintptr_t address = 0;
-	size_t size = 0;
-};
-
-std::vector<dlinfo_t> libraries;
-
-// taken form aixxe's cstrike-basehook-linux
-bool GetLibraryInformation(const char* library, uintptr_t* address, size_t* size)
-{
-	if (libraries.size() == 0) {
-		dl_iterate_phdr([](struct dl_phdr_info* info, size_t, void*) {
-			dlinfo_t library_info = {};
-
-			library_info.library = info->dlpi_name;
-			library_info.address = info->dlpi_addr + info->dlpi_phdr[0].p_vaddr;
-			library_info.size = info->dlpi_phdr[0].p_memsz;
-
-			libraries.push_back(library_info);
-
-			return 0;
-		},
-			nullptr);
-	}
-
-	for (const dlinfo_t& current : libraries) {
-		if (!strcasestr(current.library, library))
-			continue;
-
-		if (address)
-			*address = current.address;
-
-		if (size)
-			*size = current.size;
-
-		return true;
-	}
-
-	return false;
-}
 
 void* Memory::RelativeToAbsolute(void* addr)
 {
