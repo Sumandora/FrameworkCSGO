f3837fd51645549f804134cefee3505838deda5d
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-05-20 23:33:13 +0200

    We don't require root permissions here if you set ptrace_scope to 0

--- Debug.sh
+++ Debug.sh
@@ -16,19 +16,6 @@ if ! [ -x "$(command -v $DEBUGGER)" ]; then
 	exit 1
 fi
 
-# Set the SU variable on the cmdline to use e.g. doas
-SU="${SU:=sudo}"
-if ! [ $(id -u) = 0 ]; then
-	echo "Using '$SU' for upgrading privileges"
-	if ! [ -x "$(command -v $SU)" ]; then
-		echo "$SU does not exist"
-		echo "Install it or set the SU variable to a replacement"
-		exit 1
-	fi
-else
-	SU=""
-fi
-
 csgo_pid=$(pidof csgo_linux64)
 if [ -z "$csgo_pid" ]; then
 	echo "CS:GO can't be found, is the game running?"
@@ -36,11 +23,7 @@ if [ -z "$csgo_pid" ]; then
 fi
 
 lib_name="lib$(cat ProjectName).so"
-$SU cp Build-Debug/$lib_name /usr/lib64/
 
-# https://www.kernel.org/doc/Documentation/security/Yama.txt
-$SU sysctl -w kernel.yama.ptrace_scope=2 # Only allows root to inject code. This is temporary until reboot.
-
-$SU $DEBUGGER -p $csgo_pid \
-	-ex "call ((void*(*)(char*, int)) dlopen)(\"/usr/lib64/$lib_name\", 1)" \
+$DEBUGGER -p $csgo_pid \
+	-ex "call ((void*(*)(char*, int)) dlopen)(\"$(pwd)/Build-Debug/$lib_name\", 1)" \
 	-ex "continue"
