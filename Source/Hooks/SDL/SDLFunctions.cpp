cefc633515aa9036849003f42b8b222174f60750
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-09-03 11:14:57 +0200

    Rewrite SDLHook to not use dlsym, since the library got linked against SDL

--- Source/Hooks/SDL/SDLFunctions.cpp
+++ Source/Hooks/SDL/SDLFunctions.cpp
@@ -1,17 +1,13 @@
 #include "SDLFunctions.hpp"
 
-#include <dlfcn.h>
-
 // TODO Mutex
 
 void Hooks::SDL::hook()
 {
-	void* handle = dlmopen(LM_ID_BASE, "libSDL2-2.0.so.0", RTLD_NOW | RTLD_NOLOAD | RTLD_LOCAL);
-	DestroyWindow::hook = new SDLHook(handle, "SDL_DestroyWindow", reinterpret_cast<void*>(DestroyWindow::hookFunc));
-	WarpMouseInWindow::hook = new SDLHook(handle, "SDL_WarpMouseInWindow", reinterpret_cast<void*>(WarpMouseInWindow::hookFunc));
-	SwapWindow::hook = new SDLHook(handle, "SDL_GL_SwapWindow", reinterpret_cast<void*>(SwapWindow::hookFunc));
-	PollEvent::hook = new SDLHook(handle, "SDL_PollEvent", reinterpret_cast<void*>(PollEvent::hookFunc));
-	dlclose(handle);
+	DestroyWindow::hook = new SDLHook(reinterpret_cast<void*>(SDL_DestroyWindow), reinterpret_cast<void*>(DestroyWindow::hookFunc));
+	WarpMouseInWindow::hook = new SDLHook(reinterpret_cast<void*>(SDL_WarpMouseInWindow), reinterpret_cast<void*>(WarpMouseInWindow::hookFunc));
+	SwapWindow::hook = new SDLHook(reinterpret_cast<void*>(SDL_GL_SwapWindow), reinterpret_cast<void*>(SwapWindow::hookFunc));
+	PollEvent::hook = new SDLHook(reinterpret_cast<void*>(SDL_PollEvent), reinterpret_cast<void*>(PollEvent::hookFunc));
 }
 
 void Hooks::SDL::unhook()
